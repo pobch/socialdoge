645a3a76dcbba7a7a9dea60a30afcd9b2acf2599
Pob <590650@gmail.com>
2019-12-22 17:25:13 +0700

    get data from fireStore

--- socialdoge-functions/functions/src/index.ts
+++ socialdoge-functions/functions/src/index.ts
@@ -1,8 +1,30 @@
 import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+type Scream = {
+  userHandle: string
+  body: string
+}
+
+admin.initializeApp()
 
 export const hello = functions.https.onRequest((request, response) => {
   response.send('Hello from PoB!')
+})
+
+export const getScreams = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection('screams')
+    .get()
+    .then(docs => {
+      let result: Scream[] = []
+
+      docs.forEach(doc => {
+        result.push(doc.data() as Scream)
+      })
+
+      res.json(result)
+    })
+    .catch(err => console.error(err))
 })
